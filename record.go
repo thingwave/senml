3f31415e546ed27697f4357219a3001859610493
Jens Eliasson <jens.eliasson@gmail.com>
2022-01-02 12:52:49 +0100

    code cleanup

--- record.go
+++ record.go
@@ -34,7 +34,7 @@ type SenML interface {
         Validate(records SenMLMessage) error
 }
 
-func Validate(records SenMLMessage) error {
+func Validate(records SenMLMessage, ipsoMode bool) error {
         var bn string = ""
 
         if len(records) == 0 {
@@ -52,33 +52,34 @@ func Validate(records SenMLMessage) error {
                         record.Bt = time.Now().Unix()
                 }
 
-                /*if record.N == "" {
+                if record.N == "" {
                         record.N = bn
                 } else {
                         record.N = bn + record.N
-                }*/
+                }
 
-                if strings.HasPrefix(record.Bn, "urn:dev:mac:") {
+                if strings.HasPrefix(record.N, "urn:dev:mac:") {
                         fmt.Printf("bn(MAC): %s, n: %s\n", record.Bn, record.N)
                         parts := strings.Split(record.Bn, ":")
-                        //for i, part := range parts {
                         fmt.Printf("\tMAC: %s\n", parts[3])
-                        //}
-                } else if strings.HasPrefix(record.Bn, "urn:dev:sn:") {
+                } else if strings.HasPrefix(record.N, "urn:dev:sn:") {
 
-                } else if strings.HasPrefix(record.Bn, "urn:sys:name:") {
+                } else if strings.HasPrefix(record.N, "urn:sys:name:") {
                         fmt.Printf("bn(SYS): %s, n: %s\n", record.Bn, record.N)
                 }
-                parts := strings.Split(record.N, "/")
-                major, _ := strconv.Atoi(parts[0])
-                channel, _ := strconv.Atoi(parts[1])
-                minor, _ := strconv.Atoi(parts[2])
-                fmt.Printf("MAJOR: %d, MIDDLE: %d, MINOR: %d\n", major, channel, minor)
 
-                fmt.Printf("%v, %+v\n", i, record)
-                /*if record.Bn == nil {
-                        return errors.New("Missing tags")
-                }*/
+                if ipsoMode {
+                  parts := strings.Split(record.N, "/")
+                  major, _ := strconv.Atoi(parts[0])
+                  channel, _ := strconv.Atoi(parts[1])
+                  minor, _ := strconv.Atoi(parts[2])
+                  fmt.Printf("MAJOR: %d, MIDDLE: %d, MINOR: %d\n", major, channel, minor)
+
+                  fmt.Printf("%v, %+v\n", i, record)
+                  /*if record.Bn == nil {
+                          return errors.New("Missing tags")
+                  }*/
+                }
         }
 
         return nil
