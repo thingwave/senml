1aa1df9bf332a3d9d5e98801088dd505c31b3344
Jens Eliasson <jens.eliasson@gmail.com>
2022-01-02 12:00:07 +0100

    added validation func

--- record.go
+++ record.go
@@ -2,6 +2,11 @@ package senml
 
 import (
 	//"encoding/json"
+  "errors"
+  "fmt"
+  "strconv"
+  "strings"
+  "time"
 )
 
 type SenMLRecord struct {
@@ -24,3 +29,57 @@ type SenMLRecord struct {
 }
 
 type SenMLMessage []SenMLRecord
+
+type SenML interface {
+        Validate(records SenMLMessage) error
+}
+
+func Validate(records SenMLMessage) error {
+        var bn string = ""
+
+        if len(records) == 0 {
+                return errors.New("Empty array")
+        }
+
+        for i, record := range records {
+                if record.Bn != "" {
+                        bn = record.Bn
+                } else {
+                        record.Bn = bn
+                }
+
+                if record.Bt == nil { // record.T something.. check RFC
+                        record.Bt = time.Now().Unix()
+                }
+
+                /*if record.N == "" {
+                        record.N = bn
+                } else {
+                        record.N = bn + record.N
+                }*/
+
+                if strings.HasPrefix(record.Bn, "urn:dev:mac:") {
+                        fmt.Printf("bn(MAC): %s, n: %s\n", record.Bn, record.N)
+                        parts := strings.Split(record.Bn, ":")
+                        //for i, part := range parts {
+                        fmt.Printf("\tMAC: %s\n", parts[3])
+                        //}
+                } else if strings.HasPrefix(record.Bn, "urn:dev:sn:") {
+
+                } else if strings.HasPrefix(record.Bn, "urn:sys:name:") {
+                        fmt.Printf("bn(SYS): %s, n: %s\n", record.Bn, record.N)
+                }
+                parts := strings.Split(record.N, "/")
+                major, _ := strconv.Atoi(parts[0])
+                channel, _ := strconv.Atoi(parts[1])
+                minor, _ := strconv.Atoi(parts[2])
+                fmt.Printf("MAJOR: %d, MIDDLE: %d, MINOR: %d\n", major, channel, minor)
+
+                fmt.Printf("%v, %+v\n", i, record)
+                /*if record.Bn == nil {
+                        return errors.New("Missing tags")
+                }*/
+        }
+
+        return nil
+}
